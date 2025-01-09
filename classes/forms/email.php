c0231b1e29da07d37bcd8c661f67b18695241d96
Patrick Thibaudeau <thibaud@yorku.ca>
2025-01-08 22:59:37 -0500

    Replace $unit_select (previously $unitselect) to an SQL query to capture department unit and campus

--- classes/forms/email.php
+++ classes/forms/email.php
@@ -29,36 +29,28 @@ class email_form extends \moodleform
 
         $messageTypes = \local_etemplate\email::get_messagetype_nicename();
 
-        $units = new units();
-        $units = $units->get_records();
-        $departments = new departments();
-        $departments = $departments->get_records();
-        $unitselect = [];
+// Get all departments with unit and campus information
+        $unit_sql = "Select
+                        od.id,
+                        od.unit_id,
+                        od.name As department,
+                        ou.name As unit,
+                        oc.name As campus
+                    From
+                        {local_organization_dept} od Inner Join
+                        {local_organization_unit} ou On ou.id = od.unit_id Inner Join
+                        {local_organization_campus} oc On oc.id = ou.campus_id
+                    Order By
+                        campus,
+                        unit,
+                        department";
+        $units = $DB->get_records_sql($unit_sql);
 
-        foreach ($units as $check){
-            $unit = new unit($check->id);
-            //check if user belongs to unit (NOT just department)
-            if (\local_organization\base::has_capability('local/etemplate:create',$context,$USER->id,true,$check->id, 'UNIT')
-                || is_siteadmin($USER->id)){
-                $unitselect[$unit->get_id()] = $unit->get_name();
-            }
-        }
-        foreach ($departments as $check){
-            $dep = new department($check->id);
-            $depunitid = $dep->get_unit_id();
-            $depunit = new unit($depunitid);
-            //check if user belongs to unit OR department
-            if (\local_organization\base::has_capability('local/etemplate:create', $context, $USER->id, true, $depunit->get_id() . '_' . $check->id, 'DEPARTMENT')
-                ||
-                \local_organization\base::has_capability('local/etemplate:create', $context, $USER->id, true, $dep->get_unit_id(), 'UNIT')
-                || is_siteadmin($USER->id)) {
-                $unitselect[$depunit->get_id() . '_' . $check->id] = $depunit->get_name() . " / " . $dep->get_name();
-            }
-        }
+        $unit_select = [];
 
-        uasort($unitselect, function($a, $b) {
-            return strcasecmp($a, $b);
-        });
+        foreach($units as $unit) {
+            $unit_select[$unit->id] = $unit->campus .  ' / ' . $unit->unit . ' / ' . $unit->department;
+        }
 
 
         $langs = get_string_manager()->get_list_of_translations();
@@ -103,7 +95,7 @@ class email_form extends \moodleform
             'select',
             'unit',
             get_string('unit', 'local_etemplate'),
-	    $unitselect
+	    $unit_select
         );
         $mform->addHelpButton(
             'unit',
