a3f83353ad289e47ad7324287df9c11371e72ac4
thibaud <thibaud@yorku.ca>
2025-11-24 23:29:57 -0500

    Implemented new logic for view assigned templates.

--- version.php
+++ version.php
@@ -1,8 +1,8 @@
 <?php
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version = 2025092700;
+$plugin->version = 2025112400;
 $plugin->component = 'local_etemplate';
-$plugin->release = '0.0.1';
+$plugin->release = '1.0.0';
 $plugin->requires = 2019052000;
-$plugin->maturity = MATURITY_ALPHA;
+$plugin->maturity = MATURITY_STABLE;
